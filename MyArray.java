e63785ba37f0cafbfa2fb08c76651233c9281381
sophiag03 <145294217+sophiag03@users.noreply.github.com>
2023-10-02 21:32:14 -0400

    Add files via upload

---
+++ MyArray.java
@@ -0,0 +1,346 @@
+/* File: MyArray
+ * Date: 3 October 2023
+ * Author:  Sophia Guilbeault 
+ * Purpose: Creating and manipulating a MyArray object
+ */
+
+import java.util.*;                    
+
+public class MyArray  {
+
+    private static final int SENTINEL = -999;  // signalizes end of input
+    private static final int DEFAULT_SIZE = 20; 
+    private static final int LOWER = 10;
+    private static final int UPPER = 50;
+
+    private int[] arr; //MyArray object
+    private int numElements = 0; // number of elements entered into an array
+    private int sum = 0;
+    private int min = 0;
+    private int max = 0;
+    private double avg = 0;
+    private double mid = 0;
+    Scanner input = new Scanner(System.in);
+
+// CONSTRUCTORS
+
+    /* CONSTRUCTOR
+     * 
+     * if no input...
+     * initializes a new MyArray object with the default size
+     * 
+     */
+    public MyArray() {
+       this.arr = new int[DEFAULT_SIZE];
+       this.numElements = 0;
+       inputElements();
+    }
+
+    /* CONSTRUCTOR
+     * 
+     * with integer input...
+     * takes in an integer and initializes a MyArray object
+     * that is the length of the inputted integer
+     * 
+     * then asks user to input integers if wanted
+     * 
+     */
+    public MyArray(int n) {
+        if(n < 0)
+            throw new IllegalArgumentException();
+        this.arr = new int[n];
+        numElements = 0;
+        inputElements();
+    }
+
+     /* CONSTRUCTOR 
+     * 
+     * with array input...
+     * takes in an array and initializes the MyArray object
+     * as a copy of the inputted array
+     * 
+     */
+    public MyArray(int[] arr) {
+        if(arr == null)
+            throw new IllegalArgumentException();
+        this.arr = new int[arr.length];
+        for(int i = 0; i < arr.length; i++) {
+            this.arr[i] = arr[i];
+            this.numElements += 1;
+        }
+        computeStatistics();
+    }
+
+//METHODS
+    
+    /*method inputElements
+     * 
+     * asks the user for input and cycles elements through the
+     * validInput method and adds them to the MyArray object
+     * if warranted 
+     * computes statistics after final inputs 
+     * 
+     */
+    public void inputElements() {
+        System.out.println("Enter up to " + this.arr.length + " integers between " + LOWER + " and " + UPPER + " inclusive. Enter " + SENTINEL + " to end user input: ");
+        int element = input.nextInt();
+
+        recurElements(element);
+
+        computeStatistics();
+    }
+
+    /*method recurElements - helper method
+     * 
+     * used during the inputElements method to
+     * continue the input reader
+     * 
+     */
+    public void recurElements(int e) {
+
+        if(validInput(e) && numElements < this.arr.length) {
+            this.arr[numElements] = e;
+            numElements ++;
+            recurElements(input.nextInt());
+        }
+        else if (e == -999)
+            input.close();
+        else { 
+            recurElements(input.nextInt());
+        }
+
+    }
+
+    /*method validInput
+     * 
+     * takes in a number and tests to see if it is between
+     * the LOWER and UPPER bounds 
+     * before allowing it to be added to the object
+     * 
+     */
+    public static boolean validInput(double num) {
+        boolean bool = false;
+        if (num >= LOWER && num <= UPPER){
+            bool = true;
+        }
+        return bool;
+    }
+
+    /* method computeStatistics
+     * 
+     * calculates the statistics of the MyArray object
+     * 
+     */
+    public void computeStatistics() {
+        if(numElements == 0) {
+            //SOMETHING?
+        }
+        //SUM
+        for (int i = 0; i < numElements; i++) {
+            sum += arr[i];
+        }
+        //MIN
+        for (int i = 0; i < numElements - 1; i++) {
+            min = arr[i];
+            if (arr[i] > arr[i+1])
+                min = arr[i+1];
+        }
+        //MAX
+        for (int i = 0; i < numElements - 1; i++) {
+            max = arr[i];
+            if (arr[i] < arr[i+1])
+                max = arr[i+1];
+        }
+        //AVG
+        for (int i = 0; i < numElements; i++) {
+            avg += arr[i];
+        }
+        avg = avg/numElements;
+
+        //MID
+        if (numElements % 2 != 0) {
+            int pos = numElements/2;
+            mid = arr[pos];
+        }
+        if (numElements % 2 == 0) {
+            double pos1 = numElements/2;
+            double pos2 = pos1 - 1;
+            mid = (pos1 + pos2)/2;
+        }            
+    }
+
+    /* method toString
+     * 
+     * returns the MyArray object as a string
+     * includes only the user inputted numbers, not the entire array
+     * 
+     */
+    public String toString() {
+        int[] newArr = new int[this.numElements];
+        for (int i = 0; i < newArr.length; i++)
+            newArr[i] = this.arr[i];
+        return Arrays.toString(newArr);
+    }
+
+    /*method numOccurrences
+     * 
+     * takes in an int
+     * cycles through elements in the MyArray object and
+     * returns how many times the integers occurrs in the array
+     * 
+     */
+
+    public int numOccurrences(int n) {
+        int occurr = 0;
+        for (int i = 0; i < this.arr.length; i ++) {
+            if (this.arr[i] == n)
+                occurr += 1;
+        }
+        return occurr;
+    }
+
+    /*method insert
+     * 
+     * takes in an integer and the position
+     * tests to see if the position is within the 
+     * currently filled portion of the MyArray object
+     * if so, inserts the integer and returns true
+     * 
+     * otherwise returns false
+     * 
+     */
+    public boolean insert(int n, int position) {
+        boolean bool = false;
+        if (position >= numElements || position < 0) // position outside filled portion
+            bool = false; 
+        else if (numElements == this.arr.length) // arr is full
+            bool = false;
+        else{ 
+            bool = true;
+            int[] temp = new int[this.arr.length];
+            for(int i = 0; i < position; i++) {
+                temp[i] = this.arr[i];
+            }
+            temp[position] = n;
+            for(int i = position+1; i < this.arr.length; i++) {
+                temp[i] = this.arr[i-1];
+            }
+            computeStatistics();
+        }
+        return bool;
+    }
+
+    /*method remove
+     * 
+     * takes in a position and if a valod entry removes the entry 
+     * and returns the value removed
+     * 
+     * if not valid, returns -1
+     */
+    public int remove(int position) {
+        int retInt = -1;
+        if (position > 0 && position < numElements) {
+            retInt = this.arr[position];
+            int[] temp = new int[this.arr.length - 1];
+            for(int i = 0; i < position; i++) {
+                temp[i] = this.arr[i];
+            }
+
+            for (int i = position + 1; i < this.arr.length; i++) {
+                temp[i-1] = this.arr[i];
+            } 
+            this.arr = temp;
+            numElements -= 1;
+            computeStatistics();
+        }
+        return retInt;
+    }
+
+    /*method grow
+     * 
+     * takes in an integer n and "grows" the MyArray 
+     * object by n
+     * 
+     * returns false if n <=0
+     * 
+     */
+    public boolean grow(int n) {
+        boolean bool = false;
+        if (n <= 0) {
+            bool = false;
+        }
+        else {
+            bool = true;
+            int[] temp = new int[this.arr.length + n];
+            for (int i = 0; i < this.arr.length; i++) {
+                temp[i] = this.arr[i];
+            }
+            this.arr = temp;
+        }
+        return bool;
+    }
+
+    /* method computeHistogram
+     * 
+     * creates a string representation of the MyArray object
+     * using * and returns the string 
+     * 
+     */
+    public String computeHistogram() {
+        String s = "";
+        for (int i = 0; i < this.arr.length; i++) {
+            for (int x = 0; x < this.arr[i]; x++)
+                s += "*";
+            s += "\n";
+        }
+        return s;
+    }
+
+//ACCESSORS
+
+    //returns the MyArray object
+    public int[] getArr(){
+        return this.arr;
+    }
+
+    // return sum
+    public int getSum() {
+        return sum;
+    }
+
+    //return min
+    public int getMin() {
+        return min;
+    }
+    
+    //return max
+    public int getMax() {
+        return max;
+    }
+
+    //return avg
+    public double getAvg() {
+        return avg;
+    }
+
+    //return mid
+    public double getMid() {
+        return mid;
+    }
+
+    public static void main(String [] args) {
+
+        System.out.println("\nUnit Test for MyArray.\n");
+        //Scanner input = new Scanner(System.in);
+        int[] a = {3, 6, 7, 9, 11, 23};
+        MyArray mine = new MyArray(a);
+        System.out.println(mine.toString());
+        System.out.println(mine.getAvg());
+        System.out.println(mine.getSum());
+        System.out.println(mine.getMid());
+        System.out.println(mine.remove(2));
+        System.out.println(mine.toString());        
+	// TBA
+
+    }
+}
